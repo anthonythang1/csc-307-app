3ba7adbf21a8b41a46c2ec7adb756af38e53f537
anthonythang1 <anthonythang64@yahoo.com>
2026-04-14 15:53:35 -0700

    IE3 - task 3 completed

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -31,6 +31,26 @@ useEffect(() => {
     });
 }, []);
 
+function postUser(person) {
+  const promise = fetch("Http://localhost:8000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(person),
+  });
+
+  return promise;
+}
+
+function updateList(person) {
+  postUser(person)
+    .then(() => setCharacters([...characters, person]))
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
   return (
     <div className="container">
       <Table characterData={characters} removeCharacter={removeOneCharacter} />
