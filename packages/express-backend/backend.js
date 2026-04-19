d0eb71c38e8cf8cec5f34e14e3315fabb4d40f01
anthonythang1 <anthonythang64@yahoo.com>
2026-04-19 13:30:57 -0700

    IE4 - step 4: incorporated new .js files and removed helper functions with user-service functions

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,6 +1,7 @@
 // backend.js
 import express from "express";
 import cors from "cors";
+import userService from "./services/user-service.js";
 
 const app = express();
 const port = 8000;
@@ -16,124 +17,64 @@ app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
 
-const users = {
-  users_list: [
-    {
-      id: "xyz789",
-      name: "Charlie",
-      job: "Janitor",
-    },
-    {
-      id: "abc123",
-      name: "Mac",
-      job: "Bouncer",
-    },
-    {
-      id: "ppp222",
-      name: "Mac",
-      job: "Professor",
-    },
-    {
-      id: "yat999",
-      name: "Dee",
-      job: "Aspring actress",
-    },
-    {
-      id: "zap555",
-      name: "Dennis",
-      job: "Bartender",
-    },
-  ],
-};
-
-const findUserByName = (name) => {
-  return users["users_list"].filter((user) => user["name"] === name);
-};
-
-const findUserByJob = (job) => {
-  return users["users_list"].filter((user) => user["job"] === job);
-}
-
-const findUserByNameAndJob = (name, job) => {
-  return users["users_list"].filter((user) => user["name"] === name && user["job"] === job);
-}
-
 app.get("/users", (req, res) => {
   const name = req.query.name;
   const job = req.query.job;
 
-    if (name != undefined && job != undefined) { // Find user by name and job
-    let result = findUserByNameAndJob(name, job);
-    result = { users_list: result };
-    res.send(result);
-  }
-  else if (name != undefined) {              // Find user by name
-    let result = findUserByName(name);
-    result = { users_list: result };
-    res.send(result);
-  } 
-    else if (job != undefined) {              // Find user by job
-    let result = findUserByJob(job);
-    result = { users_list: result };
-    res.send(result);
-  }
-    else {                                  // No query parameters, return all users
-    res.send(users);
-  }
+  userService
+    .getUsers(name, job)
+    .then((users) => {
+      res.send({ users_list: users });
+    })
+    .catch((error) => {
+      res.status(500).send("Error retrieving users");
+    });
 });
-
-const findUserById = (id) =>
-  users["users_list"].find((user) => user["id"] === id);
 
 app.get("/users/:id", (req, res) => {
-  const id = req.params["id"]; //or req.params.id
-  let result = findUserById(id);
-  if (result === undefined) {
-    res.status(404).send("Resource not found.");
-  } else {
-    res.send(result);
-  }
+  const id = req.params.id;
+
+  userService
+    .findUserById(id)
+    .then((user) => {
+      if (user === null) {
+        res.status(404).send("Resource not found.");
+      } else {
+        res.send(user);
+      }
+    })
+    .catch((error) => {
+      res.status(500).send("Error retrieving user");
+    });
 });
-
-const generateUserId = () => {
-  return Math.random().toString(36).substr(2, 9);
-};
-
-const addUser = (user) => {
-  const id = generateUserId();
-  const userWithId = {
-    id: id,
-    name: user.name,
-    job: user.job
-  };
-  users["users_list"].push(userWithId);
-  return userWithId;
-};
 
 app.post("/users", (req, res) => {
   const userToAdd = req.body;
-  const newUser = addUser(userToAdd);
-  res.status(201).send(newUser);
-});
 
-const delUser = (id) => {
-    const index = users["users_list"].findIndex((user) => user["id"] === id); 
-  if (index !== -1){            // User found
-    users["users_list"].splice(index, 1);
-    return true;
-  }
-  else {                  // User not found     
-    return false;
-  }
-};
+  userService
+    .addUser(userToAdd)
+    .then((newUser) => {
+      res.status(201).send(newUser);
+    })
+    .catch((error) => {
+      res.status(500).send("Error adding user");
+    });
+});
 
 app.delete("/users/:id", (req, res) => {
   const userToDel = req.params.id;
-  const deleted = delUser(userToDel);
-  if (deleted) {
-    res.status(204).send();
-  } else {
-    res.status(404).send("Resource not found.");
-  }
+
+  userService
+    .removeUserById(userToDel)
+    .then((deletedUser) => {
+      if (deletedUser === null) {
+        res.status(404).send("Resource not found.");
+      } else {
+        res.status(204).send();
+      }
+    })
+    .catch((error) => {
+      res.status(500).send("Error deleting user");
+    });
 });
 
