cfcd5f44190c2a88b1422b4bf8a0a4e7da0872f8
anthonythang1 <anthonythang64@yahoo.com>
2026-04-29 12:47:33 -0700

    updated refresh issue

--- packages/react-frontend/src/Table.jsx
+++ packages/react-frontend/src/Table.jsx
@@ -15,7 +15,7 @@ function TableHeader() {
 function TableBody(props) {
   const rows = props.characterData.map((row, index) => {
     return (
-      <tr key={index}>
+      <tr key={row.id}>
         <td>{row.name}</td>
         <td>{row.job}</td>
         <td>{row.id}</td>
